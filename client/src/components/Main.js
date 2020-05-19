d47abc6af21c2c53ad873166ca26c6c84363ebb7
Parr <tedblount01@gmail.com>
2020-05-19 03:30:19 -0500

    made pic smaller. changed word

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -7,7 +7,7 @@ function Main(props) {
         <div className="row col-8 main">
             <p>Ted Web World envisions a future where communication isnt just made easier, but immersive and impacting.
             We see ourselves as constantly pushing the limits of our own comfort zone, in search of amazing ideas for web based solutions.
-            Ted Web World wants to open up people's imaginative mind, and help them interact with our clients on a more dedicated level.
+            Ted Web World wants to open up people's imaginative mind, and help them interact with clients on a more dedicated level.
             The only thing stopping us from bringing people together, is a lack of communication.
             We offer solutions in helping you connect with your customer, client, or affiliate.
             Reach out to the community with our award winning design.</p>
