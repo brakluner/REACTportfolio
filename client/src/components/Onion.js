2022d9ba35d265dc7f0006664280526152667c9b
Parr <tedblount01@gmail.com>
2020-05-19 00:43:44 -0500

    added color

--- client/src/components/Onion.js
+++ client/src/components/Onion.js
@@ -7,13 +7,13 @@ console.log(path)
 
 function Onion(props) {
     if (path === '/'){
-  return (<div><h2>Home</h2><a href='/contact'>Contact</a><br></br>
+  return (<div className="onion"><h2>Home</h2><a href='/contact'>Contact</a><br></br>
   <a href='/portfolio'>Portfolio</a></div>);
 } else if (path === '/contact') {
-    return (<div><h2>Contact</h2><a href='/'>Home</a><br></br>
+    return (<div className="onion"><h2>Contact</h2><a href='/'>Home</a><br></br>
     <a href='/portfolio'>Portfolio</a></div>);
 } else if (path === '/portfolio') {
-    return (<div><h2>Portfolio</h2><a href='/contact'>Contact</a><br></br>
+    return (<div className="onion"><h2>Portfolio</h2><a href='/contact'>Contact</a><br></br>
     <a href='/'>Home</a></div>)
 }
 }
