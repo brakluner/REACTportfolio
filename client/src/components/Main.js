f5afa3b5810332249616047bdc7225c03de5ecb4
Parr <tedblount01@gmail.com>
2020-05-19 00:24:08 -0500

    changed writing some

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -7,10 +7,10 @@ function Main(props) {
         <div className="row col-8 main">
             <p>Ted Web World envisions a future where communication isnt just made easier, but immersive and impacting.</p>
             <p>We see ourselves as constantly pushing the limits of our own comfort zone, in search of amazing ideas for web based solutions.</p>
-            <p>Ted Web World wants to open up people's imaginative mind, and help them interact with our clients on a more intimate level.</p>
+            <p>Ted Web World wants to open up people's imaginative mind, and help them interact with our clients on a more dedicated level.</p>
             <p>The only thing stopping us from bringing people together, is a lack of communication.</p>
-            <p>We offer soltions in helping you connect with your customer, client, or affiliate.</p>
-            <p>Reach out to your neighbors and fellow citizens, with our award winning design.</p>
+            <p>We offer solutions in helping you connect with your customer, client, or affiliate.</p>
+            <p>Reach out to the community with our award winning design.</p>
         </div>
         );
 }
